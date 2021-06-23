ab3d86faebabf8f0f93616768931a0f89d15631c
mtkrtk <mtkrtk@gmail.com>
2021-06-23 23:53:36 +0900

    initial commit

---
+++ target/raspberrypi_pico_gcc/target_pcb.h
@@ -0,0 +1,15 @@
+/*
+ * pcb.hのターゲット依存部（RP2040用）
+ *
+ * $Id: target_pcb.h 210 2020-02-07 10:55:38Z ertl-komori $
+ */
+
+#ifndef TOPPERS_TARGET_PCB_H
+#define TOPPERS_TARGET_PCB_H
+
+/*
+ * コアで共通な定義（チップ依存部は飛ばす）
+ */
+#include "core_pcb.h"
+
+#endif /* TOPPERS_TARGET_CCB_H */
